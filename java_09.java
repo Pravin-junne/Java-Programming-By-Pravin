3781988a9d00dd428c6d22b09fa3594a02e5fc4d
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-07-03 09:22:16 +0530

    Addition of two number

---
+++ java_09.java
@@ -0,0 +1,14 @@
+//Addition of two number
+import java.util.*;
+public class java_09 {
+    public static void main(String args[]){
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:- ");
+        a = sc.nextInt();
+        System.out.print("Enter the second number:- ");
+        b = sc.nextInt();
+        c = a+b; //logic
+        System.out.print("Addition of two number:- "+c);
+    }
+}
