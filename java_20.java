98b8d5241e61063583d3ea69d03e4e6d1be23c4c
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-07-03 22:04:08 +0530

    print the o to n number by while loop

---
+++ java_20.java
@@ -0,0 +1,15 @@
+//0 to n number print
+import java.util.*;
+public class java_20 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a, i=0;
+        System.out.print("Enter the number: ");
+         a = sc.nextInt();
+
+        while(i<=a){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
