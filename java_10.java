65596a84fd0eb2f71ba06f563125ca29d89cac67
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-07-03 09:30:24 +0530

    Subtraction of two number

---
+++ java_10.java
@@ -0,0 +1,15 @@
+//substraction of two number
+import java.util.*;
+public class java_10 {
+    public static void main(String args[]){
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:- ");
+        a = sc.nextInt();
+        System.out.print("Enter the second number:- ");
+        b = sc.nextInt();
+
+        c = a-b; //logic 
+        System.out.print("Result:- "+c);
+    }
+}
