7bbb77efa2d38c16c7d617e82f6c8effd936122c
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-07-03 09:36:03 +0530

    Multiplication of two number

---
+++ java_11.java
@@ -0,0 +1,16 @@
+//Multiplication of two number
+import java.util.*;
+public class java_11 {
+    public static void main(String args[]){
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:- ");
+        a = sc.nextInt();
+        System.out.print("Enter the second number:- ");
+        b = sc.nextInt();
+        
+        c = a*b;
+        System.out.print("Result:- "+c);
+
+    }    
+}
