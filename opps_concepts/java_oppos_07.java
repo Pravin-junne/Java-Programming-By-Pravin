06cafb476277b360bb6d385268391f98605531ca
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-08-19 00:39:36 +0530

    Non-paramiterized constractor

---
+++ opps_concepts/java_oppos_07.java
@@ -0,0 +1,12 @@
+public class java_oppos_07 {
+    public static void main(String args[]){
+        Student s1 = new Student();
+    }
+}
+
+//student class
+class Student{
+    Student(){
+        System.out.println("non-paramitraized constractor");
+    }
+}
